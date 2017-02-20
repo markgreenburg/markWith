ec89e1eea584b7401cd5d6e06ffc521d6cf1632a
Dvshah13 <dvshah13@gmail.com>
2017-02-20 13:49:14 -0600

    Added modules and db basics

--- models/Document.js
+++ models/Document.js
@@ -2,7 +2,29 @@
  * Mongoose models and classmethods for interacting with Documents
  */
 
-module.exports = {
-    Document: Document,
-    otherStuff: "etc..."
+// module.exports = {
+//     Document: Document,
+//     otherStuff: "etc..."
+// };
+
+module.exports = function Documents (docName, collaborators, createdAt, lastUpdated) {
+    this.docName = docs;
+    this.collaborators = collaborators;
+    this.createdAt = createdAt;
+    this.lastUpdated = lastUpdated;
+    this.test = function() {
+        console.log('Document Name: ' + this.docName + ' collaborators: ' + this.collaborators);
+    };
 };
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+const documentSchema = new Schema({
+    docName: { type: String, required: true },
+    collaborators: { type: String, required: true },
+    createdAt: { type: Date, required: true },
+    lastUpdated: { type: Date, required: true, default: Date.now }
+    });
+
+const Doc = mongoose.model('Doc', documentSchema);
--- models/User.js
+++ models/User.js
@@ -1,8 +1,34 @@
 /**
  * Mongoose models and classmethods for interacting with Users
  */
+//
+// module.exports = {
+//     User: User,
+//     otherStuff: "etc..."
+// };
 
-module.exports = {
-    User: User,
-    otherStuff: "etc..."
+module.exports = function User (fName, lName, email, password, createdAt, lastLogin) {
+    this.fName = fName;
+    this.lName = lName;
+    this.email = email;
+    this.password = password;
+    this.createdAt = createdAt;
+    this.lastLogin = lastLogin;
+    this.test = function() {
+        console.log(this.fName + ' ' + this.email + ' ' + this.lastLogin);
+    };
 };
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    fName: { type: String, required: true },
+    lName: {type: String, required: true },
+    email: {type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    createdAt: { type: Date, required: true },
+    lastLogin: { type: Date, required: true, default: Date.now }
+    });
+
+const User = mongoose.model('User', userSchema);
